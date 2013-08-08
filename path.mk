978897bb17fbcd48006822b77fe0e93d54b7d733
Higher <liuxiaofei333@gmail.com>
2013-08-08 15:34:42 +0800

    move the lib that used commonly(the folder base and net) to the individual folder

---
+++ path.mk
@@ -0,0 +1,8 @@
+PROJECT_ROOT_DIR=/root/lxf/se
+LIB_DIR=/root/lxf/lib
+BASE_DIR=$(LIB_DIR)/base
+NET_DIR=$(LIB_DIR)/net
+
+COMPILE_OPTION= -g -Wall -Werror
+#If you want load libary you can add this varibles
+#LOAD_LIB=-lpthread -lboost
